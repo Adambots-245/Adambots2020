3615f5caebe793e04977725d8e90700eb21d7fb2
EthanRylko <ethan.rylko@gmail.com>
2020-01-29 19:26:27 -0500

    Hacked test code into robot

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,13 +7,17 @@
 
 package frc.robot;
 
+import org.opencv.core.*;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
     public static final int FL_TALON = 0;
@@ -23,6 +27,22 @@ public final class Constants {
     public static final int INTAKE_MOTOR_PORT = 6;
     
 	public static final int INTAKE_SPEED = 1;
-	public static final int OUTTAKE_SPEED = -1;
+    public static final int OUTTAKE_SPEED = -1;
+    
+    // Vision Constants
+    public static final int CAM_NUMBER = 0;
+    public static final int IMG_WIDTH = 320;
+    public static final int IMG_HEIGHT = 240;
+    public static final int HOR_FOV_DEGREES = 60;
+    public static final double HOR_DEGREES_PER_PIXEL = (double)HOR_FOV_DEGREES / IMG_WIDTH;
+    public static final int IMG_HOR_MID = IMG_WIDTH / 2;
+    public static final int DRIVER_STATION_FPS = 6;
+    public static final int PROCESSING_FPS = 30;
+
+    // Colors are (B, G, R) (Don't ask me)
+    public static final Scalar RED = new Scalar(0, 0, 255);
+    public static final Scalar GREEN = new Scalar(0, 255, 0);
+    public static final Scalar BLUE = new Scalar(255, 0, 0);
+
 }
 
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,9 @@
 
 package frc.robot;
 
+import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -22,12 +24,21 @@ public class Robot extends TimedRobot {
 
   private RobotContainer m_robotContainer;
 
+  private VisionProcessor vision;
+  private Thread visionThread;
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
   public void robotInit() {
+    // Starts vision thread
+    vision = new VisionProcessor();
+    visionThread = vision.getVisionThread();
+    visionThread.setDaemon(true);
+    visionThread.start();
+
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
@@ -42,6 +53,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
+    SmartDashboard.putNumber("ANGLE", vision.getAngle());
     // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
     // commands, running already-scheduled commands, removing finished or interrupted commands,
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
