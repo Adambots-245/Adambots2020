e5661fc45bafdabd7ba921764e78c096b97d3bf3
Cannicide <41762964+Cannicide@users.noreply.github.com>
2020-02-08 14:36:17 -0500

    Added base dash245 code

---
+++ src/components/dash245.js
@@ -0,0 +1,34 @@
+/**
+ * SETTINGS SHOULD BE SET BY TEAM MEMBERS AHEAD OF TIME FOR ALL DASHBOARD VALUES
+ */
+
+var settings = {
+
+    dashboard: {
+
+        //Values to be put on the dashboard as small, sidebar elements
+        small: [
+            
+            //Sample Format for Dashboard Elements:
+
+
+            /** 
+             * {
+             *     name: "Gyro",
+             *     type: "number"
+             * }
+             * 
+             */
+        ],
+
+        //Values to be put on the dashboard as medium-sized, main elements
+        medium: [
+
+        ]
+
+    }
+
+}
+
+
+
