4101c0ed3dcca6cfe6b08c0c06d0c82cf27e3138
Debrini Sarkar <dsarka4659@rcs-k12.us>
2020-02-16 13:48:19 -0500

    Added code for blaster hood pneumatic (in blaster subsystem and backboard toggle command).

--- src/main/java/frc/robot/subsystems/BlasterSubsystem.java
+++ src/main/java/frc/robot/subsystems/BlasterSubsystem.java
@@ -2,9 +2,8 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -18,7 +17,6 @@ public class BlasterSubsystem extends SubsystemBase {
 
     public BlasterSubsystem() {
         super();
-
         BlasterMotor = new WPI_TalonFX(Constants.BLASTER_MOTOR_PORT);
         Backboard = new Solenoid(Constants.RAISE_BLASTER_HOOD_SOL_PORT);
     }
@@ -26,6 +24,17 @@ public class BlasterSubsystem extends SubsystemBase {
     public void output(double speed) {
         BlasterMotor.set(ControlMode.PercentOutput, speed);
     }
+
+    public void toggleBackboard() {
+        if (Backboard.get()) {
+            Backboard.set(false);
+            SmartDashboard.putString("Backboard set to ", "near yeeting. ");
+        } else { 
+            Backboard.set(true);
+            SmartDashboard.putString("Backboard set to ", "far yeeting. ");
+        }
+    }
+
 
     public void periodic() {
         // This method will be called once per scheduler run
